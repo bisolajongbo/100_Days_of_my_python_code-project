7783151833abf8d984a6e78de7c02e22ac94c613
bisolajongbo <bisolajongbo@gmail.com>
2026-05-18 14:34:07 +0100

    Day-01:PRINTING TO THE CONSOLE IN PYTHON

---
+++ Day-01/Band_Name_Generator.py
@@ -0,0 +1,6 @@
+#BAND NAME GENERATOR
+print("Welcome to Band Name Generator")
+Name=input("What is  Your Name?")
+City=input("What city did you grow up?")
+Pet=input("What is your pet name?")
+print("Your Band Name could be "+City+""+Pet)
